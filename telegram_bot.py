d36e8f5f9e2af1c7266f3609df7f7bfa3bc8ee89
intrening <wol87@ya.ru>
2020-11-25 10:58:39 +0300

    add readme

--- telegram_bot.py
+++ telegram_bot.py
@@ -213,9 +213,9 @@ def get_database_connection():
     """
     global _database
     if _database is None:
-        database_password = os.getenv("DATABASE_PASSWORD")
-        database_host = os.getenv("DATABASE_HOST")
-        database_port = os.getenv("DATABASE_PORT")
+        database_password = os.getenv("REDIS_PASSWORD")
+        database_host = os.getenv("REDIS_HOST")
+        database_port = os.getenv("REDIS_PORT")
         _database = redis.Redis(host=database_host, port=database_port, password=database_password)
     return _database
 
